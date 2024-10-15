44704754191d5137d0bfa7929d73863278a5906e
alexquar <alexanderquarrie@gmail.com>
2024-10-15 12:56:57 -0400

    error page

---
+++ src/app/_error.tsx
@@ -0,0 +1,30 @@
+import { error } from 'console';
+import React from 'react'
+import Link from 'next/link';
+
+interface ErrorProps {
+  statusCode: number;
+  errorMessage: string;
+  redirectLink: string;
+  redirectPlace: string;
+}
+
+export default function Error({ statusCode, errorMessage, redirectLink, redirectPlace }: ErrorProps) {
+  return (
+    <main className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
+    <div className="text-center">
+      <p className="text-base font-semibold text-secondaryBrand">{statusCode}</p>
+      <h1 className="mt-4 text-3xl font-bold tracking-tight text-accentBrand sm:text-5xl">Whoops, an Error Occured :(</h1>
+      <p className="mt-6 text-base leading-7 text-textBrand">{errorMessage}</p>
+      <div className="mt-10 flex items-center justify-center gap-x-6">
+        <Link
+          href={redirectLink}
+          className="rounded-xl bg-primaryBrand px-3.5 py-2.5 text-sm font-semibold hover:text-primaryBrand text-white shadow-sm border hover:bg-backgroundBrand border-primaryBrand focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Go to {redirectPlace}
+        </Link>
+      </div>
+    </div>
+  </main>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-
+import Error from "./_error";
 import { LatestPost } from "~/app/_components/post";
 import { getServerAuthSession } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
@@ -7,14 +7,16 @@ import { api, HydrateClient } from "~/trpc/server";
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
   const session = await getServerAuthSession();
-
+  const error = true
   void api.post.getLatest.prefetch();
-  await new Promise((resolve) => setTimeout(resolve, 100000));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   return (
     <HydrateClient>
+      {error ? <Error redirectLink="/" redirectPlace="home" errorMessage="Could not fetch" statusCode={400} /> :
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         to come
       </main>
+      } 
     </HydrateClient>
   );
 }
