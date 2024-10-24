1543b40ad2bd197c91017c1d5f903b7022fef574
alexquar <alexanderquarrie@gmail.com>
2024-10-24 18:13:38 -0400

    community screen

--- src/app/communities/[id]/page.tsx
+++ src/app/communities/[id]/page.tsx
@@ -20,7 +20,7 @@ if (isNaN(numericId)) {
 }
 
   return (
-    <div className="py-24 sm:py-32">
+    <div className="py-24 h-[85vh] place-content-center sm:py-32">
     <div className="mx-auto max-w-7xl px-6 lg:px-8">
       <div className="mx-auto flex justify-between w-full lg:mx-0">
         <div>
@@ -38,23 +38,29 @@ if (isNaN(numericId)) {
       </div>
     
       <div className="mx-auto flex flex-col items-center sm:flex-row sm:justify-between mt-10 w-full gap-x-8 border-t border-gray-200 pt-8">
-        <div className="flex flex-col sm:flex-row gap-y-4 sm:gap-x-4">
-        <span>
+        <div className="flex flex-col sm:flex-row gap-y-4 sm:gap-x-2">
+        <span className="flex flex-col sm:flex-row sm:border-e sm:hidden md:flex  sm:border-accentBrand sm:pe-2">
+          <p className="sm:me-2 mb-2 sm:mb-0">Created:</p>
           {community.createdAt.toDateString()}
         </span>
-        <span>
+        <span className="flex flex-col sm:flex-row sm:border-e sm:border-accentBrand sm:pe-2">
+        <p className="sm:me-2 mb-2 sm:mb-0">Located in:</p>
           {community.locationCommunity}
         </span>
-        <span>
+        <span className="flex flex-col sm:flex-row sm:border-e sm:hidden lg:flex sm:border-accentBrand sm:pe-2">
+        <p className="sm:me-2 mb-2 sm:mb-0">Last Change:</p>
           {community.updatedAt.toDateString()}
         </span>
-        <span>
+        <span className="flex flex-col sm:hidden lg:flex sm:flex-row sm:border-e sm:border-accentBrand sm:pe-2">
+        <p className="sm:me-2 mb-2 sm:mb-0"># Members:</p>
           {community.numberOfMembers}
         </span>
-        <span>
+        <span className="flex flex-col sm:flex-row">
+        <p className="sm:me-2 mb-2 sm:mb-0">Contact Adress:</p>
+
         {
           community.ownerEmail
-        }
+}
         </span>
         </div>
         <span className="bg-secondaryBrand text-white py-2 px-4 mt-4 sm:mt-0 hover:bg-secondaryBrand/50 rounded-full">
