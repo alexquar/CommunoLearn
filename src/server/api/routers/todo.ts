fee237200de01b4bc68838461bd9417515a92440
alexquar <alexanderquarrie@gmail.com>
2024-10-26 20:08:06 -0400

    todo routes

--- src/server/api/routers/todo.ts
+++ src/server/api/routers/todo.ts
@@ -1,67 +1,82 @@
+import { use } from "react";
 import { z } from "zod";
 import {
   createTRPCRouter,
   protectedProcedure,
   publicProcedure,
 } from "~/server/api/trpc";
-
+import { Stage } from "@prisma/client";
 export const todoRouter = createTRPCRouter({
-  
-  getCommunities: publicProcedure
-    .query(async({ ctx }) => {
-      return {
-        communities: await ctx.db.community.findMany(
-            {
-                where: {
-                    private : false
-                },
-                take: 10
-            }
-        ),
-      };
-    }),
+  //get a todo
+  getTodoById: publicProcedure
+  .input(z.object({ id: z.number() }))
+  .query(async({ ctx, input }) => {
+    return await ctx.db.todo.findUnique({
+      where: {
+        id: input.id
+      },
+    })
+  }),
 
-    getCommunity: publicProcedure
-    .input(z.object({ id: z.number() }))
-    .query(async({ ctx, input }) => {
-      return await ctx.db.community.findUnique({
-          where: {
-              id: input.id
-          }
-      })
-    }),
+  //delete a todo
+  deleteTodo: protectedProcedure
+  .input(z.object({ id: z.number() }))
+  .mutation(async({ ctx, input }) => {
+    return await ctx.db.todo.delete({
+      where: {
+        id: input.id
+      }
+    })
+  }),
 
-    newCommunity: publicProcedure
-    .input(z.object({ 
-        name: z.string().min(1), 
-        aboutCommunity: z.string().min(1), 
-        private: z.boolean(),
-        ownerEmail: z.string().min(1),
-        password: z.string(),
-        locationCommunity: z.string().min(1),
-        sloganCommunity: z.string().min(1),
-        communityType: z.string().min(1)
-    }))
-    .mutation(async({ ctx, input }) => {
-      return await ctx.db.community.create({
-          data: {
-              name: input.name,
-              aboutCommunity: input.aboutCommunity,
-              private: input.private ?? null,
-              password: input.password ?? null,
-              locationCommunity: input.locationCommunity,
-              sloganCommunity: input.sloganCommunity,
-              numberOfMembers: 1,
-              //need a user here
-              createdBy: 
-              {
-                  connect: { id: 'cm2avbnnf0000buxc4t44yo3p' }
-              },
-              ownerEmail: input.ownerEmail,
-              communityType: input.communityType,
-              //will need to do actual user eventually
+  //create a new todo
+  newTodo: protectedProcedure
+  .input(z.object({ 
+    title: z.string().min(1), 
+    content: z.string().min(1), 
+    userId: z.string().min(1),
+    projectId: z.number()
+  }))
+  .mutation(async({ ctx, input }) => {
+    return await ctx.db.todo.create({
+      data: {
+        title: input.title,
+        content: input.content,
+        createdBy: {
+          connect: {
+            id: input.userId
           }
-      })
-    }),
+        },
+        Project: {
+          connect: {
+            id: input.projectId
+          }
+        }
+      }
+    })
+  }),
+
+  //update a todo
+  updateTodo: protectedProcedure
+  .input(z.object({ 
+    id: z.number(),
+    title: z.string().min(1), 
+    content: z.string().min(1), 
+    stage: z.nativeEnum(Stage),
+    done: z.boolean()
+  }))
+  .mutation(async({ ctx, input }) => {
+    return await ctx.db.todo.update({
+      where: {
+        id: input.id
+      },
+      data: {
+        title: input.title,
+        content: input.content,
+        stage: input.stage,
+        done: input.done
+      }
+    })
+  }),
 
 });
