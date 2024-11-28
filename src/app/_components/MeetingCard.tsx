f8eb232dcf2e98be771abce52f9ee8687357ab55
alexquar <alexanderquarrie@gmail.com>
2024-11-28 08:52:47 -0500

    meeting cards

--- src/app/_components/MeetingCard.tsx
+++ src/app/_components/MeetingCard.tsx
@@ -1,14 +1,30 @@
 import React from 'react'
 import { type Meeting } from '@prisma/client'
+import Link from 'next/link'
 export default function MeetingCard({meeting}:{meeting: Meeting}) {
   return (
-    <div className='max-w-sm h-56 bg-white shadow-xl border border-accentBrand rounded-2xl p-4 text-accentBrand'>
-      <h1>
+    <div className='max-w-sm h-64 bg-white shadow-xl border text-left border-accentBrand rounded-2xl p-4 flex flex-col gap-y-4 text-accentBrand'>
+      <h1 className='text-3xl truncate font-bold'>
         {meeting.title}
       </h1>
-      <p>
-        {meeting.content}
+      <p className='text-sm text-semibold truncate text-textBrand'>
+       About: {meeting.content}
       </p>
+      <p className='text-sm text-semibold truncate text-textBrand'>
+       {meeting.inPerson? "Location":"Link"}: {meeting.meetingLocationOrLink}
+      </p>
+      <p className='text-sm text-semibold truncate text-textBrand'>
+        When: {new Date(meeting.meetingTime.toISOString()).toLocaleString()}
+      </p>
+    <Link href={`/meeting/${meeting.id}`} className='px-10 flex flex-row gap-x-1 hover:bg-primaryBrand/70 py-3 rounded-3xl bg-primaryBrand text-white w-fit'>
+    <p>View More</p>
+    <span className='my-auto'>
+    <svg  className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+        </svg>
+    </span>
+    </Link>
+
     </div>
   )
 }
--- src/app/_components/MeetingGrid.tsx
+++ src/app/_components/MeetingGrid.tsx
@@ -3,7 +3,7 @@ import { type Meeting } from '@prisma/client'
 import MeetingCard from './MeetingCard'
 export default function MeetingGrid({meetings}:{meetings: Meeting[]}) {
   return (
-    <div className='grid grid-col-1 md:grid-cols-2 xl:grid-cols-4 gap-4 lg:gap-8'>
+    <div className='grid grid-col-1 mx-auto justify-center md:grid-cols-2 xl:grid-cols-3 gap-4 lg:gap-8'>
       {
         meetings.map((meeting) => (
           <React.Fragment key={meeting.id}>
