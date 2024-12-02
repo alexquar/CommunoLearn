0f4e7d8bf22a3515118b94fdd786858f4934db1e
alexquar <alexanderquarrie@gmail.com>
2024-12-02 13:08:17 -0500

    fix vercel fail

--- src/app/_components/_comments/Comment.tsx
+++ src/app/_components/_comments/Comment.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 "use client";
 import type { CommentWithRelations } from "~/types/commentTypes";
 import userIcon from "../../../../public/user.svg";
--- src/app/messaging/page.tsx
+++ src/app/messaging/page.tsx
@@ -5,7 +5,7 @@ export default function page() {
     <div className='text-center my-10 px-10 md:my-24 flex flex-col gapy-y-8 text-3xl mx-auto text-accentBrand font-bold'>
         <h1>Coming Soon</h1>
         <p className='text-base font-semibold text-textBrand'>
-            If you're seeing this message it means that messaging has not been made available to the public yet. Check back soon!
+            If you&apos;re seeing this message it means that messaging has not been made available to the public yet. Check back soon!
         </p>
     </div>
   )
